96ea3742bba115f429361209018ce166f67026f0
kels7 <hiketabo@gmail.com>
2022-11-13 22:13:25 +0000

    add

---
+++ src/lib/components/copyToClipboard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher<{ copy: string; fail: never}>();
+    //export let text: string;
+
+    const copy = (e: any)=>{
+        let text = e.target.dataset.offerId
+        navigator.clipboard.writeText(text).then(
+            ()=>dispatch("copy", text),
+            (e)=>dispatch("fail")
+        );
+    }
+</script>
+
+<slot {copy}/>
