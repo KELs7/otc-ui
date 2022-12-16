a13be4f38926daecc02aef9aec91b0cdaf0d20df
kels7 <hiketabo@gmail.com>
2022-12-16 22:45:03 +0000

    update with needed functions

--- src/utils/connections.utils.ts
+++ src/utils/connections.utils.ts
@@ -1,26 +1,45 @@
 import WalletController from 'lamden_wallet_controller';
+import { get } from 'svelte/store';
 import { connectionRequest } from '../configs'
-import { lwc_store } from '../stores';
+import { lwc_store, vk_store, toast_store } from '../stores';
 
 
-export const handleWalletInfo = (walletInfo)=> console.log("www")
-export const handleTxnInfo = (txInfo)=> console.log("tttt")
+export const handleWalletInfo = (wInfo)=> {
+    if (wInfo.errors){
+        wInfo.errors.forEach(err => {
+            if(err=="You must be an authorized dApp to send this message type. Send 'lamdenWalletConnect' event first to authorize.")return
+            toast_store.set({show: true, error:true, message:err})
+        })
+        setTimeout(()=>{
+            toast_store.set({show: false})
+        }, 4000)
+        return
+    }
+    vk_store.set(wInfo.wallets[0])
+}
+//export const handleTxnInfo = (txInfo)=> console.log("tttt")
 
 export const initWalletController= ()=>{
     const lwc = new WalletController(connectionRequest);
-
-    lwc_store.set(lwc)      
+    lwc_store.set(lwc)     
 }
 
-
-
-export const isWalletInstalled = (lwc)=>{
-    lwc.walletInstalled()
+export const isWalletInstalled = ()=>{
+    const lwc = get(lwc_store)
+    
+    if(lwc===null){
+        toast_store.set({show: true, error:true, message:"Connection request not initialised!" })
+        return
+    }
+    lwc.walletIsInstalled()
         .then(installed=>{
             if(installed){}
             else {
-                //send "install wallet" info to toast
-                console.log("install wallet")
+                toast_store.set({show: true, error:true, message:"install wallet" })
             }
         })
+}
+
+export const getCurrentWalletInfo = ()=>{
+    get(lwc_store).getInfo()
 }
