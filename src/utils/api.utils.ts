0c0a0551a962135cf35ad5923839908abeefc343
kels7 <hiketabo@gmail.com>
2022-12-16 22:44:03 +0000

    update with needed functions

--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -1,52 +1,111 @@
 import axios from "axios"
-import type { I_Offer, I_ProcessedOffer } from "../types";
-import { offer_store } from '../stores';
+import type { I_Offer, I_FormatttedForTable } from "../types";
+import { blockservice, connectionRequest, supportedTokens } from "../configs";
+import {  balance_tau_store } from '../stores'
+
 //mock data
-import offers from '../lib/mock-data/offers.json';
+//import bs_data from '../lib/mock-data/offers.json';
 
-const local_hostnames = [
-    '0.0.0.0', 'localhost', '127.0.0.1'
-]
+async function walkThroughBsUrls(contract: string, variable: string){
+    //TODO: provide a timeout to avoid getting stuck when all blockservices are offline
+    for (let bs of blockservice){
+        const res = await axios.get(`https://${bs}/current/all/${contract}/${variable}`) 
+        if(res.data) return res.data
+    }
 
-//const isProd = () => !local_hostnames.includes(window.location.hostname)
-
-//const base_url = isProd() ? 'api/' : `${constructTestingUrl()}:2001/`
-
-// function constructTestingUrl() {
-//     const url = `${window.location.protocol}//${window.location.hostname}`
-//     console.log(url)
-//     return url
-// }
-
-export async function syncOffers(): Promise<void> {
+}
+   
+export async function syncOffers(): Promise<any> {
     try {
-        //const offers = (await axios.get(`${base_url}all_offers`)).data as I_Offer[]
+        //const bs_data = (await axios.get(`https://${blockservice[0]}/current/all/${connectionRequest.contractName}/data`)).data as I_Offer[]
 
-         
-        const f = await processOffers(offers.offers)
+        const bs_data =  await walkThroughBsUrls(connectionRequest.contractName, "data")
 
-        offer_store.set(f)
-        //console.log(offers.offers)
+        const processed: I_Offer[]  = processBlockserviceData(bs_data)
+
+        const formatted : I_FormatttedForTable[] = formatForTable(processed)
+
+        return formatted
 
     } catch (err) {
         console.log(err)
     }
 }
 
-export async function processOffers(offers: I_Offer[]): Promise<I_ProcessedOffer[]> {
-    let offerArray: any[] = [];
-    for (let f of offers){
-        //let offer_id = `${f.offer_id.slice(0, 3)}. . .${f.offer_id.slice(61, 64)}`
-        let maker = `${f.maker.slice(0, 4)}. . .${f.maker.slice(61, 64)}`
-        let offer = `${f.offer_amount} ${f.offer_token}`
-        let take = `${f.take_amount} ${f.take_token}`
+function processBlockserviceData(bs_data: any): I_Offer[]{
+    const offersData : any = bs_data.con_otc001.data
+    const ids: string[] = Object.keys(offersData)
 
-        offerArray.push({
-            offer_id: f.offer_id,
-            maker: maker,
-            offer: offer,
-            take: take
-        })
+    let processed = []
+    for(let id of ids){
+        
+        const { 
+            fee, maker, 
+            offer_amount, offer_token, 
+            state, take_amount, 
+            take_token, taker
+        }   = offersData[id]
+
+        //gather only OPEN offers
+        if(state === 'OPEN'){
+           
+            const offerObj: I_Offer ={ 
+                offer_id: id, 
+                fee, maker, 
+                offer_amount, offer_token,
+                state, take_amount, 
+                take_token, taker
+            }
+           
+            processed.push(offerObj)
+        }
     }
-    return offerArray
+
+    return processed
+}
+
+function formatForTable(processed: I_Offer[]): I_FormatttedForTable[] {
+    let formatted = []
+    
+    for(let f of processed){
+        const offer_id = f.offer_id
+        const maker = `${f.maker.slice(0, 4)}. . .${f.maker.slice(61, 64)}`
+        const offer = `${getValueFromFixed(f.offer_amount)} ${assignTokenSymbol(f.offer_token)}`
+        const take = `${getValueFromFixed(f.take_amount)} ${assignTokenSymbol(f.take_token)}`
+
+        formatted.push({ offer_id, maker, offer, take })
+    }
+    
+    return formatted
+}
+
+function getValueFromFixed(isItFixed: any): number{
+    let value: number;
+    if(isItFixed===null)return 0
+    isItFixed.__fixed__?value = Number(isItFixed.__fixed__): value = Number(isItFixed)
+    return value
+}
+
+export async function getTauBalance(vk: string) {
+    try {
+
+        for (let bs of blockservice){
+            const balance: any = (await axios.get(`https://${blockservice}/current/one/currency/balances/${vk}`)).data
+            if(balance){
+                balance_tau_store.set(getValueFromFixed(balance.value).toFixed(8))
+                console.log(balance)
+                return
+            }
+        }
+        
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+function assignTokenSymbol(contract: string): string{
+    let st = Object.keys(supportedTokens)
+    for(let t of st){
+        if(contract === t)return supportedTokens[t]
+    }
 }
