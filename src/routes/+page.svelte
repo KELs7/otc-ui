02948499272bddcbb2148ddd2d40817cfbca1312
kels7 <hiketabo@gmail.com>
2022-12-28 10:40:47 +0000

    show loader when data not loaded yet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import CopyToClipboard from '$lib/components/copyToClipboard.svelte';
+  import Loader from '$lib/components/loader-table-page.svelte';
   
   export let data: any;
   const { offers } = data
@@ -14,35 +15,48 @@
 
 </script>
 
-<!--TODO: show loader when nothing is being loaded-->
+{#if offers === undefined}
+  <div class="loader-bkg">
+    <Loader/>
+  </div>
+  
+{:else}
 
-<table class="u-full-width">
-  <thead>
-    <tr>
-      <th>Offer id</th>
-      <th>Maker</th>
-      <th>Offer</th>
-      <th>Take</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each offers as offer_data}
+  <table class="u-full-width">
+    <thead>
       <tr>
-          <td>
-            <CopyToClipboard on:copy={handleSuccessfulCopy} on:fail={handleFailedCopy} let:copy>
-              <button data-offer-id={offer_data.offer_id} on:click={copy}>copy id</button>
-            </CopyToClipboard>
-            
-          </td>
-          <td>{offer_data.maker}</td>
-          <td>{offer_data.offer}</td>
-          <td>{offer_data.take}</td>
+        <th>Offer id</th>
+        <th>Maker</th>
+        <th>Offer</th>
+        <th>Take</th>
       </tr>
-    {/each}
-  </tbody>
-</table>
+    </thead>
+    <tbody>
+      {#each offers as offer_data}
+        <tr>
+            <td>
+              <CopyToClipboard on:copy={handleSuccessfulCopy} on:fail={handleFailedCopy} let:copy>
+                <button data-offer-id={offer_data.offer_id} on:click={copy}>copy id</button>
+              </CopyToClipboard>
+              
+            </td>
+            <td>{offer_data.maker}</td>
+            <td>{offer_data.offer}</td>
+            <td>{offer_data.take}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+
+{/if}
+
 
 <style>
+  .loader-bkg{
+    position: relative; 
+    height: 50vh;
+    width: 100%;
+  }
   table{
     color: var(--font-color);
     /*background-color: var(--form-color);*/
