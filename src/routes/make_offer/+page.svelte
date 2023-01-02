fee4eee03258ee91c4707cb5c69512357507c5b5
kels7 <hiketabo@gmail.com>
2023-01-02 10:57:47 +0000

    add approval txn logic

--- src/routes/make_offer/+page.svelte
+++ src/routes/make_offer/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import type { I_TransactionObj } from "../../types";
     import {
-        makeOffer, 
-        takeOffer, 
-        cancelOffer
+        makeOfferTxInfo, 
+        takeOfferTxInfo, 
+        cancelOfferTxInfo,
+        approveOTCcontractTxInfo
     } from '../../configs'
     import { sendTransaction} from '../../utils/txn.utils'
     import { encodeNumber } from '../../utils/encoding.utils'
+    import { checkInputs } from '../../utils/validation.util';
     import { lwc_store, form_button_inactive_store } from '../../stores'
     //THOUGHT: probably move to config
     const spinnerGif = "icons8-spinning-circle.gif"
@@ -14,23 +16,36 @@
     import Loader from '$lib/components/loader.svelte'
 
     const submitOffer = async()=> {
-        let txInfo = makeOffer
-        txInfo.kwargs.offer_amount = encodeNumber(txInfo.kwargs.offer_amount)
-        txInfo.kwargs.take_amount = encodeNumber(txInfo.kwargs.take_amount)
+        makeOfferTxInfo.kwargs.offer_amount = encodeNumber(makeOfferTxInfo.kwargs.offer_amount)
+        makeOfferTxInfo.kwargs.take_amount = encodeNumber(makeOfferTxInfo.kwargs.take_amount)
+        //approval txInfo
+        approveOTCcontractTxInfo.contractName  = makeOfferTxInfo.kwargs.offer_token
+        approveOTCcontractTxInfo.kwargs.amount = makeOfferTxInfo.kwargs.offer_amount
         
         form_button_inactive_store.set({form:"makeOfferForm"});
-        await sendTransaction(txInfo) 
+
+        checkInputs(makeOfferTxInfo.kwargs)
+       
+        await sendTransaction(makeOfferTxInfo) 
+        await sendTransaction(approveOTCcontractTxInfo) //send approval txn
     }
 
      const submitTakeOffer = async()=> {
+        checkInputs(takeOfferTxInfo.kwargs)
+        
+        approveOTCcontractTxInfo.contractName  = takeOfferTxInfo.kwargs.offer_token
+        approveOTCcontractTxInfo.kwargs.amount = encodeNumber(takeOfferTxInfo.kwargs.offer_amount)
         
         form_button_inactive_store.set({form:"takeOfferForm"});
-        await sendTransaction(takeOffer) 
+        
+        await sendTransaction(takeOfferTxInfo) 
+        await sendTransaction(approveOTCcontractTxInfo) //send approval txn
     }
 
      const submitCancelOffer = async()=> {
+        checkInputs(cancelOfferTxInfo.kwargs)
         form_button_inactive_store.set({form:"cancelOfferForm"});
-        await sendTransaction(cancelOffer) 
+        await sendTransaction(cancelOfferTxInfo) 
     }
         
 </script>
@@ -39,22 +54,22 @@
     <div class="row">
         <div class="one-half column">
             <p>What token do you want to otc?</p>
-            <input type="text" bind:value={makeOffer.kwargs.offer_token}/>
+            <input type="text" bind:value={makeOfferTxInfo.kwargs.offer_token}/>
         </div>
         <div class="one-half column">
             <p>How much do you want to otc?</p>
-            <input type="text" bind:value={makeOffer.kwargs.offer_amount}/>
+            <input type="text" bind:value={makeOfferTxInfo.kwargs.offer_amount}/>
         </div>
     </div>
     
     <div class="row">
         <div class="one-half column">
             <p>What token do you want in exchange?</p>
-            <input type="text" bind:value={makeOffer.kwargs.take_token}/>
+            <input type="text" bind:value={makeOfferTxInfo.kwargs.take_token}/>
         </div>
         <div class="one-half column">    
             <p>How much do you want in exchange?</p>
-            <input type="text" bind:value={makeOffer.kwargs.take_amount}/>
+            <input type="text" bind:value={makeOfferTxInfo.kwargs.take_amount}/>
         </div>
     </div>
 
@@ -70,7 +85,7 @@
 
 <form on:submit|preventDefault={submitTakeOffer} >
     <p>Paste offer id to accept offer</p>
-    <input type="text" bind:value={takeOffer.kwargs.offer_id}/><br>
+    <input type="text" bind:value={takeOfferTxInfo.kwargs.offer_id}/><br>
     <button class:inactive={$form_button_inactive_store.form} class="font-color">
         {#if $form_button_inactive_store.form == "takeOfferForm"}
             <!--img class="loading" src={spinnerGif} alt="pending"/-->
@@ -83,7 +98,7 @@
 
 <form on:submit|preventDefault={submitCancelOffer}>
     <p>Paste offer id to cancel offer you made</p>
-    <input type="text" bind:value={cancelOffer.kwargs.offer_id}/><br>
+    <input type="text" bind:value={cancelOfferTxInfo.kwargs.offer_id}/><br>
     <button class:inactive={$form_button_inactive_store.form} class="font-color">
         {#if $form_button_inactive_store.form === "cancelOfferForm"}
             <!--img class="loading" src={spinnerGif} alt="pending"/-->
