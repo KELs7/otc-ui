a30abcc88968776f1301a54f5c1f7d1f93ffeee6
kels7 <hiketabo@gmail.com>
2023-03-07 18:55:44 +0000

    fix types

--- src/stores.ts
+++ src/stores.ts
@@ -1,10 +1,10 @@
 import { writable, type Writable } from 'svelte/store';
-import type { I_Offer, I_ProcessedOffer } from './types';
+// import type { I_Offer, I_ProcessedOffer } from './types';
 
 export const vk_store: Writable<null|string> = writable(null)
 export const toast_store: Writable<any> = writable({show: false, error:"", msg:""})
-export const menu_slide_store: Writable<bool> = writable(false)
+export const menu_slide_store: Writable<boolean> = writable(false)
 export const lwc_store: Writable<any|null> = writable(null)
-export const form_button_inactive_store: Writable<bool> = writable({form: ""})
-export const balance_tau_store: Writable<any> = writable(0)
+export const form_button_inactive_store: Writable<any> = writable({form: ""})
+export const balance_tau_store: Writable<Number> = writable(0)
 
