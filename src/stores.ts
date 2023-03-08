abfc4db1a7880cdde06ab43715403c2f37f05f70
kels7 <hiketabo@gmail.com>
2023-03-08 11:29:36 +0000

    change type to strings

--- src/stores.ts
+++ src/stores.ts
@@ -1,10 +1,10 @@
 import { writable, type Writable } from 'svelte/store';
 // import type { I_Offer, I_ProcessedOffer } from './types';
 
-export const vk_store: Writable<null|string> = writable(null)
+export const vk_store: Writable<string> = writable("")
 export const toast_store: Writable<any> = writable({show: false, error:"", msg:""})
 export const menu_slide_store: Writable<boolean> = writable(false)
 export const lwc_store: Writable<any|null> = writable(null)
 export const form_button_inactive_store: Writable<any> = writable({form: ""})
-export const balance_tau_store: Writable<Number> = writable(0)
+export const balance_tau_store: Writable<string> = writable("0")
 
